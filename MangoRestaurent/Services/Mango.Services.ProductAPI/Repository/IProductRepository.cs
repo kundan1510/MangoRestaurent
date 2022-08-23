dd8bbfd743440809a4a9d099435173eb725f250e
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-08-23 17:14:57 +0530

    23. Product Repository Interface

--- MangoRestaurent/Services/Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ MangoRestaurent/Services/Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -2,5 +2,9 @@
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
