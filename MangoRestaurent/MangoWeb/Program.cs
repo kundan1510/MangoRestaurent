9835ce86a7abf9fe1731a08e4d97f6b4b11b6c56
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-08-24 07:37:47 +0530

    Configure ProductService and HttpClient in Startup

--- MangoRestaurent/MangoWeb/Program.cs
+++ MangoRestaurent/MangoWeb/Program.cs
@@ -1,4 +1,15 @@
+using MangoWeb;
+using MangoWeb.Services;
+using MangoWeb.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddHttpClient<IProductService, ProductService>();
+SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
