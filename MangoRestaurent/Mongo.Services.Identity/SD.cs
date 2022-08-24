bc7fd5a5642eb58cfd6d932436c352f2aefd7a4f
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-08-24 15:18:58 +0530

    53. Configuring Clients in Identity Server

--- MangoRestaurent/Mongo.Services.Identity/SD.cs
+++ MangoRestaurent/Mongo.Services.Identity/SD.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace Mongo.Services.Identity
 {
@@ -32,6 +33,21 @@ namespace Mongo.Services.Identity
                     ClientSecrets= { new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes={ "read", "write","profile"}
+                },
+                new Client
+                {
+                    ClientId="mango",
+                    ClientSecrets= { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris={"https://localhost:7068/signin-oidc"},
+                    PostLogoutRedirectUris={"https://localhost:7068/signout-callback-oidc"},
+                    AllowedScopes=new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "mango"
+                    }
                 },
 
             };
