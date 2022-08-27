ea16d5986e2772a7fb10a7b9dba4def06db80411
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-08-27 19:28:03 +0530

    Subject and claim is not comming

--- MangoRestaurent/MangoWeb/Program.cs
+++ MangoRestaurent/MangoWeb/Program.cs
@@ -28,6 +28,8 @@ builder.Services.AddAuthentication(options =>
         options.ClientId = "mango";
         options.ClientSecret = "secret";
         options.ResponseType = "code";
+        options.ClaimActions.MapJsonKey("role", "role", "role");
+        options.ClaimActions.MapJsonKey("sub", "sub", "sub");
 
         options.TokenValidationParameters.NameClaimType = "name";
         options.TokenValidationParameters.RoleClaimType = "role";
