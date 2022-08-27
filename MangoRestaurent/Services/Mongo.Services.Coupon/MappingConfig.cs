86f8a8b9b5a8c82aab6bb69b5bc5b9f86a858abd
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-08-27 22:14:09 +0530

    Section 10 - Setup Coupon API Project

---
+++ MangoRestaurent/Services/Mongo.Services.Coupon/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace Mongo.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<ProductDto, Product>().ReverseMap();
+                //config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                //config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                //config.CreateMap<Cart, CartDto>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
