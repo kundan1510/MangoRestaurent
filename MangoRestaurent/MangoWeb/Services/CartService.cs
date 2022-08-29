3e97e8e963398e8c09b0e0945d51c7311bccbd81
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-08-29 22:39:52 +0530

    Section 12 - Checkout Method in Cart Service

--- MangoRestaurent/MangoWeb/Services/CartService.cs
+++ MangoRestaurent/MangoWeb/Services/CartService.cs
@@ -74,5 +74,16 @@ namespace MangoWeb.Services
                 AccessToken = token
             });
         }
+
+        public async Task<T> Checkout<T>(CartHeaderDto cartHeader, string token = null)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartHeader,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/checkout",
+                AccessToken = token
+            });
+        }
     }
 }
--- MangoRestaurent/MangoWeb/Services/IServices/ICartService.cs
+++ MangoRestaurent/MangoWeb/Services/IServices/ICartService.cs
@@ -10,6 +10,7 @@ namespace MangoWeb.Services.IServices
         Task<T> RemoveFromCartAsync<T>(int cartId, string token = null);
         Task<T> ApplyCoupon<T>(CartDto cartDto, string token = null);
         Task<T> RemoveCoupon<T>(string userId, string token = null);
+        Task<T> Checkout<T>(CartHeaderDto cartHeader, string token = null);
 
     }
 }
