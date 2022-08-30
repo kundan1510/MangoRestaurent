43f44f808cc9673d8a70464e6a61d7204eb75546
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-08-30 10:41:18 +0530

    129. Adding Models for Order API

---
+++ MangoRestaurent/Services/Mongo.Services.OrderAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mongo.Services.OrderAPI.Models;
+
+namespace Mongo.Services.OrderAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+
+}
