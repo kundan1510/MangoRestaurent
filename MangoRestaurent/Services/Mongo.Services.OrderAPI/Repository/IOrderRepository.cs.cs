3054c32a19c822b65401e96683670406dce3a200
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-08-30 12:10:26 +0530

    Section 14 - Order Repository Interface

---
+++ MangoRestaurent/Services/Mongo.Services.OrderAPI/Repository/IOrderRepository.cs.cs
@@ -0,0 +1,10 @@
+﻿using Mongo.Services.OrderAPI.Models;
+
+namespace Mongo.Services.OrderAPI.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader orderHeader);
+        Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+    }
+}
---
+++ MangoRestaurent/Services/Mongo.Services.OrderAPI/Repository/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using Mongo.Services.OrderAPI.Models;
+
+namespace Mongo.Services.OrderAPI.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
