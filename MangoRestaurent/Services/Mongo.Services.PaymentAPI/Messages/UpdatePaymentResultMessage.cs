010014baeb065cdad8e54ad1f93aea69669cec26
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-09-01 21:03:33 +0530

    Section 16 - Created Payment API to consume message

---
+++ MangoRestaurent/Services/Mongo.Services.PaymentAPI/Messages/UpdatePaymentResultMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Mongo.Services.PaymentAPI.Messages
+{
+    public class UpdatePaymentResultMessage
+    {
+        public int OrderId { get; set; }
+        public bool Status { get; set; }
+    }
+}
